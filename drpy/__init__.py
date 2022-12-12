28601e8689963ec4677fec3d6f4824bb89a4cadd
Andreas Olesen <dresing@live.dk>
2022-12-12 12:56:26 +0100

    :tada: Initial commit

---
+++ drpy/__init__.py
@@ -0,0 +1,15 @@
+# mypy: disable-error-code=attr-defined
+"""Tools for working with dimentionality reduction"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
